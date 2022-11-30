31cde0b29db2633bbf5194b84cc0cac3eb108af1
Diego Molina <diegoricardo.moro@gmail.com>
2022-11-30 15:09:29 -0400

    Nuevos ejercicios agregados

--- KYU_7/kyu7.js
+++ KYU_7/kyu7.js
@@ -21,3 +21,13 @@ The binary number returned should be a string.
 // function decimalToBinarie (a,b){
 //     return (a+b).toSting(2);
 // }
+
+
+
+/*
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+*/
+
+function solution(s){
+    return (s+"_").match(/.{2}/g)||[]
+}
